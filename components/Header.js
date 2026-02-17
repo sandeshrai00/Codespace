1056cd36f268bb4daa1a73c91ee75c99694e861c
sandesh rai <meromainact@gmail.com>
2026-02-17 09:31:36 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -24,7 +24,7 @@ export default function Header() {
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 hover:opacity-90 transition-all duration-300 group">
             <Image 
-              src="/img/logo.jpg" 
+              src="/img/logo.png" 
               alt="GoHoliday Logo" 
               width={140}
               height={40}
